714d5941bfa9526ebbb44dbb84ea67698cbba51c
Gosha Student <yaren042@mail.ru>
2023-03-20 18:18:00 +0500

    add winner

--- src/game.js
+++ src/game.js
@@ -33,28 +33,30 @@ class Area{
 	}
 
 	show(){
-		let str=" "
+		let str=""
 		for(let i=0; i<9; i++){
 			if((i+1)%3==0){
-				str = " "+str +" "+ this.area_Array[i].value+ " "
-				console.log(str)
-				console.log("\n")
-				str=" "
+			    str = str +" "+ this.area_Array[i].value
+			    str = str +"\n"
 			}else{
 				str = str +" "+ this.area_Array[i].value
 			}
 		}
+		console.log(str)
 		console.log("----------------------------")
 	}
 }
 
 class Game{
-	state = "in process"
+	winner
+	isFinished 
 	gameArea
 	first_gamer //{id: id, value: "X" or "O"}
 	second_gamer 
 	current_user
 	constructor(first_gamer, second_gamer){
+		this.winner = undefined
+		this.isFinished = false
 		this.first_gamer = first_gamer
 		this.second_gamer = second_gamer
 		this.current_user = first_gamer
@@ -63,28 +65,21 @@ class Game{
 
 	fillSquare(gamer, square_id){
 		try{
-			if(this.current_user.id == gamer.id){
-				let square = this.gameArea.area_Array[square_id]
-				if(square.fill(gamer.value, gamer.id)){
-					//this.checkState()
-				    this.makeTurn(gamer.id)
+			if(!this.isFinished){
+				if(this.current_user.id == gamer.id){
+					let square = this.gameArea.area_Array[square_id]
+					if(square.fill(gamer.value, gamer.id)){
+						this.checkState()
+						this.makeTurn(gamer.id)
+					}
+				}else{
+					console.log("now you cannot make turn")
+					return 0
 				}
-				// if(square.isEmpty){
-				// 	square.user_id = gamer.id
-				// 	square.value = gamer.value
-				// 	square.isEmpty = false
-				// 	this.checkState()
-				// 	this.makeTurn(gamer.id)
-				// }else{
-				// 	console.log("square already filled")
-				// 	return 0
-				// }
 
 			}else{
-				console.log("now you cannot make turn")
-				return 0
+				console.log(`game is finished. winner is ${this.winner}`)
 			}
-
 		}catch(error){
 			console.log(error)
 		}
@@ -102,31 +97,32 @@ class Game{
 	}
 
 	checkState(){
+		let area = this.gameArea.area_Array
+
 		for(let i=0; i<3; i++){
-			if(this.gameArea[i]==this.gameArea[i+3] && this.gameArea[i]==this.gameArea[i+6]){
-				this.state="finished"
-				console.log("game finished")
+			if(area[i].value==area[i+3].value && area[i].value==area[i+6].value && area[i].isEmpty==false){
+				this.isFinished = true
+				this.winner = this.current_user.id
 				return 0
 			}
 		}
+
 		for(let i=0; i<=6; i=i+3){
-			if(this.gameArea[i]==this.gameArea[i+1] && this.gameArea[i]==this.gameArea[i+2]){
-				this.state="finished"
-				console.log("game finished")
+			if(area[i].value==area[i+1].value && area[i].value==area[i+2].value && area[i].isEmpty==false){
+				this.isFinished = true
+				this.winner = this.current_user.id
 				return 0
 			}
 		}
-		if(this.gameArea[0]==this.gameArea[4] && this.gameArea[0]==this.gameArea[8]){
-			this.state="finished"
-			console.log("game finished")
+		if(area[0].value==area[4].value && area[0].value==area[8].value && area[0].isEmpty==false){
+			this.isFinished = true
+			this.winner = this.current_user.id
 			return 0
 		}
-		else if(this.gameArea[2]==this.gameArea[4] && this.gameArea[2]==this.gameArea[6]){
-			this.state="finished"
-			console.log("game finished")
+		else if(area[2].value==area[4].value && area[2].value==area[6].value && area[2].isEmpty==false){
+			this.isFinished = true
+			this.winner = this.current_user.id
 			return 0
-		}else{
-			this.state = "in process"
 		}
 	}
 
@@ -137,16 +133,33 @@ let user1 = {id: 1, value: "O"}
 let user2 = {id: 2, value: "X"}
 
 let game = new Game(user1, user2)
-console.log(game)
+//console.log(game)
 game.fillSquare(user1,0)
 // console.log(game)
-game.fillSquare(user2,2)
+game.fillSquare(user2,7)
 
-game.fillSquare(user1,2) //already filled error
-
-game.fillSquare(user1,4)
-game.fillSquare(user2,5)
 game.fillSquare(user1,3)
-game.fillSquare(user2,8)
+
+game.fillSquare(user2,5)
+
+game.fillSquare(user1,2)
+
+game.fillSquare(user2, 6)
+
+game.fillSquare(user1, 4)
+
+game.fillSquare(user2, 8)
+
+game.fillSquare(user1, 1)
+
+game.fillSquare(user2, 8)
+
+
+// game.fillSquare(user2,8)
+// console.log(game.isFinished)
+// game.fillSquare(user1,7)
+// console.log(game.isFinished)	
+// game.fillSquare(user2,6)	
+
 
 //console.log(game)
